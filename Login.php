061a4a33911d71ef1b11521a3de581dfe81adac5
furofo <chriscellphonerepair@gmail.com>
2021-02-13 14:26:53 -0500

    added sanition wiht prepare statment and switched ot object style

--- Login.php
+++ Login.php
@@ -1,6 +1,6 @@
 
 
-<form action="Login.php" method="post">
+<form action="Login.php" method="post"> 
  <p>User Name: <input type="text" name="name" /></p>
  <p>Password: <input type="text" name="password" /></p>
  <p><input type="submit" /></p>
@@ -8,35 +8,38 @@
 
 <?php
 
-if (!empty($_POST))
+if (!empty($_POST)) // basically if submit button clicked
 {
+$servername = "localhost"; // all the variables needed to connect to database
+$username = "root";
+$password = "";
+$dbname = "crypto";
 
-    
-
-  $mysqli = mysqli_connect("localhost", "root", "", "crypto");
-  $res = mysqli_query($mysqli, "SELECT * FROM login WHERE username = '{$_POST['name']}' AND  password = '{$_POST['password']}'");
-  $affectedRows = mysqli_affected_rows($mysqli);
-  echo $affectedRows, '<br>';
-  if($affectedRows > 0)
-  {
-    $row = mysqli_fetch_row($res);
-    echo 'Valid Username  ', $row[0], ': ', $row[1], '<br>';
-  }
-
-  else{
-    echo 'Incorrect Username or password';
-  }
-
-  /*
+$conn = new mysqli($servername, $username, $password, $dbname); // this will be connection to database
+if ($conn->connect_error) { //if it doesn't connect log the error
+  die("Connection failed: " . $conn->connect_error);
+}
+else {
+  $username = $_POST['name']; // get post variables and assign them to intenral variable
+  $password = $_POST['password'];
+  $stmt = $conn->prepare("SELECT username, Password FROM login WHERE username = ? AND Password = ?"); // this is to sanitize inputs and protect agains injection attacks
+  $stmt->bind_param("ss", $username, $password);
   
-  if(!$res === FALSE) {
-      echo 'Incorrect Username or password';
+  $stmt -> execute();
+
+  $stmt->bind_result($name, $retrievedPassword);  // assign the  columns username and passowrd to variables name and passowrd
+
+ if($stmt -> fetch()) {
+    echo 'Succesful login user: ' . $name . ' Password: '. $retrievedPassword;
   }
-  else{
-  $row = mysqli_fetch_row($res);
-  echo 'Total Bought Of  ', $row[0], ': ', $row[1], '<br>';
+
+ else  {
+   echo 'Invalid login attemnpt. Please input valid username or password';
   }
-  */
+  
+
+}
+
   
   
    }
